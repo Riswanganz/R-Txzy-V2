4d2f8f77148010478355057077f3453dbe7bba66
Riswanganz <100130164+Riswanganz@users.noreply.github.com>
2022-03-07 18:33:38 +0700

    Update sewabot.js

--- plugins/sewabot.js
+++ plugins/sewabot.js
@@ -11,8 +11,8 @@ let handler = async(m, { conn }) => {
 Silahkan klik pada "List Harga" untuk melihat list.
 
 Pembayaran:
-Pulsa : 085730903853
-GoPay: 085730903853
+Pulsa : 083804343232
+GoPay: 083804343232
 `.trim()
     const button = {
         buttonText: 'List Harga',
