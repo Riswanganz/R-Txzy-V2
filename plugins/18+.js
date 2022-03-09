d3fb2833770887e010df1b1d628496426fc6a691
Riswanganz <100130164+Riswanganz@users.noreply.github.com>
2022-03-09 09:29:17 +0700

    Update 18+.js

--- plugins/18+.js
+++ plugins/18+.js
@@ -1,2 +1,23 @@
+//BOTCAHX RECODE4
 
-//ga ada bokep bokepan
+let handler  = async (m, { conn }) => {
+	
+	await m.reply('BOKEP TEREERUUUUSSS, GW DAH APUS FITUR')
+  conn.reply(m.chat,`${pickRandom(global.bokep)}`, m)
+}
+handler.help = ['filebokep']
+handler.tags = ['internet']
+handler.command = /^(filebokep)$/i
+handler.owner = false
+handler.mods = false
+handler.premium = true
+handler.group = false
+handler.private = false
+
+handler.admin = false
+handler.botAdmin = false
+
+handler.fail = null
+handler.limit = 1
+
+module.exports = handler
