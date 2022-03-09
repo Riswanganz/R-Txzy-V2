d27f5ca5d069def0d5f01ab1e129af8e42d68fef
Riswanganz <100130164+Riswanganz@users.noreply.github.com>
2022-03-09 09:45:43 +0700

    Update snk.js

--- plugins/snk.js
+++ plugins/snk.js
@@ -1,33 +1,34 @@
 let fetch = require('node-fetch')
 let handler = async(m, { conn }) => {
     let teks = `
-Kebijakan Privasi, Syarat Ketentuan dan Peraturan R-Txzy Bot
+Kebijakan Privasi, Syarat Ketentuan dan Peraturan Wanz Bot
 
 Kebijakan Privasi
-1. R-Txzy tidak akan merekam data riwayat chat user.
-2. R-Txzy tidak akan menyebarkan nomor users.
-3. R-Txzy tidak akan menyimpan media yang dikirimkan oleh users.
-4. R-Txzy tidak akan menyalah gunakan data data users.
-5. Owner R-Txzy berhak melihat data riwayat chat users.
-6. Owner R-Txzy berhak melihat status users.
-7. Owner R-Txzy dapat melihat riwayat chat, dan media yang dikirimkan users.
+1. Wanz tidak akan merekam data riwayat chat user.
+2. Wanz tidak akan menyebarkan nomor users.
+3. Wanz tidak akan menyimpan media yang dikirimkan oleh users.
+4. Wanz tidak akan menyalah gunakan data data users.
+5. Owner Wanz berhak melihat data riwayat chat users.
+6. Owner Wanz berhak melihat status users.
+7. Owner Wanz dapat melihat riwayat chat, dan media yang dikirimkan users.
 
-Peraturan R-Txzy Bot
+Peraturan Wanz Bot
 1. Users dilarang menelpon maupun memvideo call nomor bot.
 2. Users dilarang mengirimkan berbagai bug, virtex, dll ke nomor bot.
 3. Users diharap tidak melakukan spam dalam penggunaan bot.
 4. Users dilarang menambahkan nomor bot secara illegal, untuk menambahkan silahkan hubungi owner.
 5. Users diharap untuk tidak menyalah gunakan fitur fitur bot.
+6. Users dilarang *KERAS!* memasukan bot ke dalam grup yang terkait porn, dan yang bersangkutan dengan kriminal
 
-Syarat Ketentuan R-Txyz Bot
+Syarat Ketentuan Wanz Bot
 1. Bot akan keluar dari group apabila sudah waktunya keluar.
-2. R-Txzy dapan mem-ban users secara sepihak terlepas dari users salah atau tidak.
-3. R-Txzy *tidak akan bertanggungjawab atas apapun yang users lakukan terhadap fitur bot.*
-4. R-Txzy akan memberlakukan hukuman: block atau ban terhadap users yang melanggar peraturan.
-5. R-Txzy bertanggung jawab atas kesalahan fatal dalam programing maupun owner.
+2. Wanz dapan mem-ban users secara sepihak terlepas dari users salah atau tidak.
+3. Wanz *tidak akan bertanggungjawab atas apapun yang users lakukan terhadap fitur bot.*
+4. Wanz akan memberlakukan hukuman: block atau ban terhadap users yang melanggar peraturan.
+5. Wanz bertanggung jawab atas kesalahan fatal dalam programing maupun owner.
 
-- AlyaXd
-- TioXd
+- Wanz-Bot
+- WaaoneOfc
 
 Peraturan: 1 Februari 2022
 `.trim()
