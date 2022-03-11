718527c7640b965b8fe4beb8f19b7b8c438c62ac
Riswanganz <100130164+Riswanganz@users.noreply.github.com>
2022-03-11 20:16:09 +0700

    Update animal.js

--- plugins/animal.js
+++ plugins/animal.js
@@ -17,7 +17,7 @@ ${usedPrefix}${command} panda
   if (!res.ok) throw `${res.status} ${res.statusText}`
   let json = await res.json()
   if (!json.image) throw json
-  conn.sendFile(m.chat, json.image, '', `${json.fact}\n\n© Animal`, m)
+  conn.sendFile(m.chat, json.image, '', `${json.fact}\n\n© Wanz`, m)
 }
 handler.help = ['animal'].map((v) => v + ' <opsi>')
 handler.tags = ['internet']
